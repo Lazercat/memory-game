7aaf957e7524c09c9245423ce6cdf6bca935d39b
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-10 04:56:01 -0500

    establishing new repository
    working from lesson 7

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Javascript
+
+console.log("Up and running!");
+
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardFour);
