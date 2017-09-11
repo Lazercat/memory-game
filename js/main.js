dcecc5e0b698775df7c44e9f2b65ffb39eed2507
Lazercat <jesseaustinlewis@gmail.com>
2017-09-10 23:41:26 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 // Javascript
 
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardsInPlay);
-
-if (cardsInPlay.length === 2){
-	console.log('User picked 2 cards');
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-
-
-} else {
-	console.log('User has not picked 2 cards.');
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+		
 }
+
+
+var flipCard = function(cardId){
+
+	cardsInPlay.push(cards[cardId]);
+	console.log('User flipped ' + cards[cardId]);
+		
+	if (cardsInPlay.length === 2){
+			checkForMatch();
+		} else {
+			
+		}
+}
+
+flipCard(0);
+flipCard(2);
